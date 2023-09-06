7f5c6437aab809390524d381fcc93171d4bd1504
alonso-estevam <alonso_estevam@outlook.com>
2023-09-05 22:31:46 -0300

    clever solution

--- src/codewars/WhatIsInBetween.java
+++ src/codewars/WhatIsInBetween.java
@@ -2,6 +2,8 @@ package codewars;
 
 import java.util.Arrays;
 
+import static java.util.stream.IntStream.rangeClosed;
+
 
 /*          WHAT IS IN BETWEEN? - 8kyu
 Complete the function that takes two integers (a, b, where a < b) and return an array
@@ -16,6 +18,8 @@ public class WhatIsInBetween {
         System.out.println(Arrays.toString(mySolution(1,4)));
 
         System.out.println(Arrays.toString(littleImprovedSolution(1,4)));
+
+        System.out.println(Arrays.toString(CleverSolution.cleverSolution(1,4)));
     }
     private static int[] mySolution(int a, int b) {
         int size = (b - a) + 1;
@@ -33,6 +37,12 @@ public class WhatIsInBetween {
             result[i - a] = i;
         }
         return result;
+    }
+
+    interface CleverSolution {
+        static int[] cleverSolution(int a, int b) {
+            return rangeClosed(a, b).toArray();
+        }
     }
 
 }
