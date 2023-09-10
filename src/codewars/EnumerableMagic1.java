a61e18736d85c9ad10b88e8c75a772de2fb25ff4
alonso-estevam <alonso_estevam@outlook.com>
2023-09-10 11:18:56 -0300

    enumerable magic #1 - solution with tradicional loop

--- src/codewars/EnumerableMagic1.java
+++ src/codewars/EnumerableMagic1.java
@@ -9,13 +9,23 @@ Create a method all which takes two params:
 and returns true if the function in the params returns true for every element in the sequence.
 Otherwise, it should return false. If the sequence is empty, it should return true, since
 technically nothing failed the test.
+    Example:    all((1, 2, 3, 4, 5), greater_than_9) -> false
+                all((1, 2, 3, 4, 5), less_than_9)    -> True
 */
 public class EnumerableMagic1 {
     public static void main(String[] args) {
 
+        boolean output = all(new int[] {10, 2, 6, 4}, i -> i % 2 == 0);
+
+        System.out.println(output);
+
     }
     public static boolean all(int[] list, IntPredicate predicate){
-        // REMINDER: a Predicate "p" is tested for argument "arg" using p.test(arg)
+        for (int i = 0; i < list.length; i++) {
+            if(! predicate.test(list[i])){
+                return false;
+            }
+        }
         return true;
     }
 }
