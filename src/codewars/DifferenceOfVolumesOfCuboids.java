d2d4587111849641f4f9d8466fb56b6ccc2b7e42
alonso-estevam <alonso_estevam@outlook.com>
2023-09-11 21:30:48 -0300

    difference of volumes of cuboids - task description

---
+++ src/codewars/DifferenceOfVolumesOfCuboids.java
@@ -0,0 +1,21 @@
+package codewars;
+
+/*  From: https://www.codewars.com/kata/58cb43f4256836ed95000f97/train/java
+    In this simple exercise, you will create a program that will take two lists of integers,
+    a and b. Each list will consist of 3 positive integers above 0, representing the dimensions
+    of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which
+    is bigger.
+
+    For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and
+    the volume of b is 20. Therefore, the function should return 8.
+*/
+public class DifferenceOfVolumesOfCuboids {
+    public static void main(String[] args) {
+
+    }
+
+    public static int findDifference(final int[] firstCuboid, final int[] secondCuboid) {
+        //your code here !!
+        return 2021;
+    }
+}
