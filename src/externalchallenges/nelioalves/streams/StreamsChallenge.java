94638c4269cbf6f26483d5df510ad3b8b0311bea
alonso-estevam <alonso_estevam@outlook.com>
2023-09-12 11:50:50 -0300

    streams challenge1 - task description and setup

---
+++ src/externalchallenges/nelioalves/streams/StreamsChallenge.java
@@ -0,0 +1,9 @@
+package externalchallenges.nelioalves.streams;
+
+/* Make a program to read a set of products from a .csv file (suppose
+  there is at least one product). Show the average price of the
+  products. Then show the names in descending order of the products
+  that have a price lower than the average price.
+  (Section 20, lesson 261 from Java POO Udemy course by Nelio Alves  */
+public class StreamsChallenge {
+}
