8800aa4e26dc1f081e4cffb7331bb10c9ee15c32
alonso-estevam <alonso_estevam@outlook.com>
2023-09-15 21:04:37 -0300

    java string reverse - task description

--- src/hackerrank/JavaStringReverse.java
+++ src/hackerrank/JavaStringReverse.java
@@ -2,6 +2,15 @@ package hackerrank;
 
 import java.util.Scanner;
 
+/* From: https://www.hackerrank.com/challenges/java-string-reverse/problem
+    A palindrome is a word, phrase, number, or other sequence of characters which reads
+    the same backward or forward. Given a string A, print Yes if it is a palindrome,
+    print No otherwise.
+    Constraints:  A will consist at most 50 lower case english letters.
+    Sample Input:    madam
+    Sample Output:   Yes
+
+*/
 public class JavaStringReverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
