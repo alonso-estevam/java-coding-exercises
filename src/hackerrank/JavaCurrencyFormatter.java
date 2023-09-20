e87ffcde6a0c0fb4ff218add47958b4f5debcf13
alonso-estevam <alonso_estevam@outlook.com>
2023-09-19 23:14:07 -0300

    java currency formatter - india locale

--- src/hackerrank/JavaCurrencyFormatter.java
+++ src/hackerrank/JavaCurrencyFormatter.java
@@ -12,16 +12,19 @@ public class JavaCurrencyFormatter {
 
         double payment = sc.nextDouble();
 
+        Locale.setDefault(Locale.US);
         NumberFormat nfUs = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat nfIndia = NumberFormat.getCurrencyInstance(new Locale ("en", "in"));
         NumberFormat nfChina = NumberFormat.getCurrencyInstance(Locale.CHINA);
         NumberFormat nfFrance = NumberFormat.getCurrencyInstance(Locale.FRANCE);
 
         String us = nfUs.format(payment);
+        String india = nfIndia.format(payment);
         String china = nfChina.format(payment);
         String france = nfFrance.format(payment);
 
         System.out.println("US: " + us);
-//        System.out.println("India: " + india);
+        System.out.println("India: " + india);
         System.out.println("China: " + china);
         System.out.println("France: " + france);
 
