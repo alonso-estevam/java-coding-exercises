74b464cc39e04c7e6a30c0ea6ad6af5ba0b0941d
alonso-estevam <alonso_estevam@outlook.com>
2023-09-19 22:25:00 -0300

    java currency formatter - pre built locale formats

---
+++ src/hackerrank/JavaCurrencyFormatter.java
@@ -0,0 +1,31 @@
+package hackerrank;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+// From: hackerrank.com/challenges/java-currency-formatter/problem
+
+public class JavaCurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double payment = sc.nextDouble();
+
+        NumberFormat nfUs = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat nfChina = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat nfFrance = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        String us = nfUs.format(payment);
+        String china = nfChina.format(payment);
+        String france = nfFrance.format(payment);
+
+        System.out.println("US: " + us);
+//        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+
+
+        sc.close();
+    }
+}
