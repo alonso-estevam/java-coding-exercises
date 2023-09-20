893735bd25265d93857dfd0dc600e2b20dfebd0c
alonso-estevam <alonso_estevam@outlook.com>
2023-09-20 15:55:24 -0300

    log levels - solution with split method

--- src/exercism/_002_LogLevels.java
+++ src/exercism/_002_LogLevels.java
@@ -3,7 +3,7 @@ package exercism;
 // From: https://exercism.org/tracks/java/exercises/log-levels
 public class _002_LogLevels {
     public static void main(String[] args) {
-        String msg = "[ERROR]: Illegal argument";
+        String msg = "[ERROR]:   Illegal argument  ";
 
         System.out.println(LogLevels.message(msg));
         System.out.println(LogLevels.logLevel(msg));
@@ -12,7 +12,7 @@ public class _002_LogLevels {
     }
     static class LogLevels {
         public static String message(String logLine) {
-            return logLine.substring(logLine.indexOf(":") + 1).strip();
+            return logLine.split("]: ")[1].strip();
         }
         public static String logLevel(String logLine) {
             return extractLog(logLine);
@@ -20,8 +20,12 @@ public class _002_LogLevels {
         public static String reformat(String logLine) {
             return String.format("%s (%s)", message(logLine), extractLog(logLine));
         }
+
+        // keeping the code DRY
         private static String extractLog(String logLine){
-            return logLine.substring(logLine.indexOf("[") + 1, logLine.lastIndexOf("]")).toLowerCase();
+            return logLine.split("]: ")[0]
+                    .substring(1)
+                    .toLowerCase();
         }
     }
 }
