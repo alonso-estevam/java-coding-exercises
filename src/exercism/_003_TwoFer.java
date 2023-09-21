2ba5843dcafd8aeaedf5c47cd2e4947c0de6c2f0
alonso-estevam <alonso_estevam@outlook.com>
2023-09-21 14:51:22 -0300

    two fer - solution

---
+++ src/exercism/_003_TwoFer.java
@@ -0,0 +1,24 @@
+package exercism;
+
+// From: https://exercism.org/tracks/java/exercises/two-fer
+public class _003_TwoFer {
+    public static void main(String[] args) {
+        String givenName = "Aline";
+        String nullName = null;
+        String emptyName = "";
+
+        System.out.println(Twofer.twofer(givenName));
+        System.out.println(Twofer.twofer(nullName));
+        System.out.println(Twofer.twofer(emptyName));
+
+    }
+
+    public class Twofer {
+        public static String twofer(String name) {
+            if(name == null || name.isEmpty()){
+                name = "you";
+            }
+            return String.format("One for %s, one for me.", name);
+        }
+    }
+}
