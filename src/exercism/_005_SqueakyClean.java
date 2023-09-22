fa37e3701b3f2e0cde8c9efe24ef30d8213260f0
alonso-estevam <alonso_estevam@outlook.com>
2023-09-22 14:57:35 -0300

    squeaky clean - improved solution

--- src/exercism/_005_SqueakyClean.java
+++ src/exercism/_005_SqueakyClean.java
@@ -15,68 +15,23 @@ public class _005_SqueakyClean {
     }
     static class SqueakyClean {
         static String clean(String identifier) {
-            for (int i = 0; i < identifier.length(); i++) {
-                if(Character.isISOControl(identifier.charAt(i))) {
-                    identifier = identifier.replace(String.valueOf(identifier.charAt(i)), "CTRL");
+            StringBuilder output = new StringBuilder();
+            boolean prevHyphen = false;
+            // Another way to iterate with a for each loop on identifier.toCharArray().
+            for (char ch : identifier.toCharArray()) {
+                if (Character.isLetter(ch) && (ch < 'α' || ch > 'ω')) {
+                    /* Instead of replacing characters in another String, which is computationally
+                     expensive, append qualifying characters to a StringBuilder. */
+                    output.append(!prevHyphen ? ch : Character.toUpperCase(ch));
+                } else if (ch == ' ') {
+                    output.append("_");
+                } else if (Character.isISOControl(ch)) {
+                    output.append("CTRL");
                 }
+                prevHyphen = ch == '-';
             }
-
-            return convertKebabToCamelCase(
-                    omitNotletters(
-                            omitLowerCaseGreekLetter(identifier)))
-                    .replace(" ", "_")
-                    .replace("-", "");
+            return output.toString();
         }
-        private static String convertKebabToCamelCase(String identifier){
-            StringBuilder sb = new StringBuilder();
-            int size = identifier.length();
-            char repetead = '0';
-            for (int i = 0; i < size; i++) {
-                if (identifier.charAt(i) != '-' && identifier.charAt(i) != repetead){
-                    sb.append(identifier.charAt(i));
-                } else {
-                    if (i + 1 < size && identifier.charAt(i) == '-'){
-                        sb.append(Character.toUpperCase(identifier.charAt(i + 1)));
-                        repetead = identifier.charAt(i + 1);
-                    }
-                }
-            }
-            return sb.toString().replace("-", "");
-        }
-        private static String omitNotletters(String identifier) {
-            return identifier.chars()
-                    .filter(c -> Character.isLetter(c)
-                            || c == '-' || c == ' ')
-                    .collect(StringBuilder::new,
-                            StringBuilder::appendCodePoint,
-                            StringBuilder::append)
-                    .toString();
-        }
-
-        private static String omitLowerCaseGreekLetter(String identifier){
-            int alfa = 'α';
-            int omega = 'ω';
-            return identifier.chars()
-                    .filter(c -> c < alfa || c > omega)
-                    .collect(StringBuilder::new,
-                            StringBuilder::appendCodePoint,
-                            StringBuilder::append)
-                    .toString();
-        }
-
     }
 }
-/*
-Another way to iterate could be with a for each loop on identifier.toCharArray().
 
-Instead of replacing characters in another String, which is computationally expensive,
-perhaps append qualifying characters to a StringBuilder.
-
-The first condition could check if it's a letter and is not a Greek letter.
-The second could check if the char is a space. The third and fourth could check if it's
-a control char or a hyphen. That would proceed from the most to the least likely condition.
-
-Although I personally don't think it lends itself well to the functional approach for
-either readability or performance, another approach might be more functional, such as
-using Arrays.stream on split with map and collect.
- */
