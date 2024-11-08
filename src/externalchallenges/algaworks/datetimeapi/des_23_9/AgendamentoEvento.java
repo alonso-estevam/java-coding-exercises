3423d02532c685bdbcfc72813d6bc1f64955f78d
Alonso Estevam <alonso_estevam@outlook.com>
2024-11-08 11:22:36 -0300

    desafio sobre localdatetime

---
+++ src/externalchallenges/algaworks/datetimeapi/des_23_9/AgendamentoEvento.java
@@ -0,0 +1,54 @@
+package externalchallenges.algaworks.datetimeapi.des_23_9;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Scanner;
+
+/*
+  Conteúdo: LocalDate, LocalTime e LocalDateTime
+
+  Desenvolva um programa que simula o agendamento de eventos. Ao ser executado, o programa
+  deve pedir a data do evento (com o input no padrão dd/MM/yyyy). Se o usuário inserir uma
+  data que não existe (como 30/30/2024), o programa deve retornar a mensagem "Data inválida.
+  Tente novamente." e solicitar de novo a data do evento. Em seguida, o programa deve pedir
+  o horário do evento (formato HH:mm), também validando se o horário é válido, e ao final
+  deve exibir a mensagem no padrão: "Evento agendado para domingo, 25 de dezembro de 2022".
+ */
+public class AgendamentoEvento {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Data do evento: ");
+        LocalDate data = obterDataAPartirDeString(sc.next());
+
+        System.out.print("Horário do evento: ");
+        LocalTime horario = obterHorarioAPartirDeString((sc.next()));
+
+
+        System.out.println(obterMensagemDataHoraFormatada(data, horario));
+
+        sc.close();
+    }
+
+    private static LocalDate obterDataAPartirDeString(String dataTexto) {
+
+        return LocalDate.parse(dataTexto, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    private static LocalTime obterHorarioAPartirDeString(String horarioTexto) {
+
+        return LocalTime.parse(horarioTexto, DateTimeFormatter.ofPattern("HH:mm"));
+    }
+
+    private static String obterMensagemDataHoraFormatada(LocalDate data, LocalTime horario) {
+
+        return String.format("Evento agendado para %s %s",
+                data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)),
+                horario.format(DateTimeFormatter.ofPattern("HH:mm")));
+    }
+}
---
+++ src/externalchallenges/algaworks/datetimeapi/des_23_9/AgendamentoEventoSolucaoAlgaworks.java
@@ -0,0 +1,63 @@
+package externalchallenges.algaworks.datetimeapi.des_23_9;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+import java.time.format.ResolverStyle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AgendamentoEventoSolucaoAlgaworks {
+
+    private static final Scanner ENTRADA = new Scanner(System.in);
+    private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/uuuu")
+            // esse estilo de resolução garante que o dia do mês seja válido para o ano-mês, rejeitando valores
+            // inválidos como 30/02/2024, mas aceitando 29/02/2024, por exemplo (2024 é bissexto).
+            // Para isso, importante trocar o yyyy por uuuu no ofPattern do DateTimeFormatter.
+            .withResolverStyle(ResolverStyle.STRICT);
+    private static final DateTimeFormatter FORMATADOR_HORARIO = DateTimeFormatter.ofPattern("HH:mm");
+    private static final DateTimeFormatter FORMATADOR_COMPLETO_BRASIL = DateTimeFormatter
+            .ofLocalizedDateTime(FormatStyle.FULL, FormatStyle.SHORT)
+            .withLocale(Locale.of("pt", "BR"));
+//            .withLocale(Locale.FRENCH); // uma alternativa é passar um Locale pré-definido
+
+    public static void main(String[] args) {
+
+        LocalDate dataEvento = requisitarData("Data do evento: ");
+        LocalTime horaEvento = requisitarHorario("Horário do evento: ");
+
+        LocalDateTime dataHoraEvento = dataEvento.atTime(horaEvento);
+
+        System.out.printf("Evento agendado para %s%n",
+                dataHoraEvento.format(FORMATADOR_COMPLETO_BRASIL));
+    }
+
+    private static LocalDate requisitarData(String descricao) {
+        while (true) {
+            try {
+                System.out.print(descricao);
+                String dataTexto = ENTRADA.nextLine();
+
+                return LocalDate.parse(dataTexto, FORMATADOR_DATA);
+            } catch (DateTimeParseException e) {
+                System.out.println("Data inválida. Tente novamente.");
+            }
+        }
+    }
+
+    private static LocalTime requisitarHorario(String descricao) {
+        while (true) {
+            try {
+                System.out.print(descricao);
+                String horarioTexto = ENTRADA.nextLine();
+
+                return LocalTime.parse(horarioTexto, FORMATADOR_HORARIO);
+            } catch (DateTimeParseException e) {
+                System.out.println("Horário inválido. Tente novamente.");
+            }
+        }
+    }
+}
