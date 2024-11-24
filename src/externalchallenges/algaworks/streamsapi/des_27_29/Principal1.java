7faba142c7ef8255b7d64cc9d63b5d67093605a3
Alonso Estevam <alonso_estevam@outlook.com>
2024-11-24 10:28:24 -0300

    desafio streams api

---
+++ src/externalchallenges/algaworks/streamsapi/des_27_29/Principal1.java
@@ -0,0 +1,20 @@
+package externalchallenges.algaworks.streamsapi.des_27_29;
+
+import externalchallenges.algaworks.streamsapi.des_27_29.comercial.ServicoDeVenda;
+import externalchallenges.algaworks.streamsapi.des_27_29.comercial.Venda;
+
+import java.util.List;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        var servicoDeVenda = new ServicoDeVenda();
+        List<Venda> vendas = servicoDeVenda.obterTodas();
+
+        for (Venda venda : vendas) {
+            if (venda.isFechada()) {
+                System.out.println(venda);
+            }
+        }
+    }
+}
---
+++ src/externalchallenges/algaworks/streamsapi/des_27_29/comercial/Cliente.java
@@ -0,0 +1,4 @@
+package externalchallenges.algaworks.streamsapi.des_27_29.comercial;
+
+public record Cliente(String nome) {
+}
